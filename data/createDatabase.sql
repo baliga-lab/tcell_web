8d10053c7275b65f92f501b3cdaba017a734fb30
cplaisier <cplaisier@systemsbiology.org>
2016-06-06 11:57:58 -0700

    Cleaned up the mysql database creation.

--- data/createDatabase.sql
+++ data/createDatabase.sql
@@ -26,61 +26,20 @@ CREATE TABLE bic_gene (
     FOREIGN KEY (gene_id) REFERENCES gene (id)
 );
 
-create table phenotypes (id integer primary key auto_increment, name varchar(50) not null, long_name varchar(50));
-
-CREATE TABLE patient (
+CREATE TABLE exp_cond (
     id integer unsigned NOT NULL AUTO_INCREMENT,
     name varchar(50),
-    phenotype_id integer references phenotypes,
     PRIMARY KEY (id),
     KEY idx_name (name)
 );
 
-CREATE TABLE bic_pat (
+CREATE TABLE bic_con (
     id integer unsigned NOT NULL AUTO_INCREMENT,
     bicluster_id integer unsigned,
-    patient_id integer unsigned,
+    exp_cond_id integer unsigned,
     PRIMARY KEY (id),
     FOREIGN KEY (bicluster_id) REFERENCES bicluster (id),
-    FOREIGN KEY (patient_id) REFERENCES patient (id)
-);
-
-CREATE TABLE replication (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    bicluster_id integer unsigned,
-    study varchar(50),
-    var_exp_fpc float,
-    var_exp_fpc_p_value float,
-    survival float,
-    survival_p_value float,
-    PRIMARY KEY (id),
-    FOREIGN KEY (bicluster_id) REFERENCES bicluster (id)
-);
-
-CREATE TABLE hallmark (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    name varchar(100),
-    PRIMARY KEY (id),
-    KEY idx_name (name)
-);
-
-CREATE TABLE bic_hal (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    bicluster_id integer unsigned,
-    hallmark_id integer unsigned,
-    PRIMARY KEY (id),
-    FOREIGN KEY (bicluster_id) REFERENCES bicluster (id),
-    FOREIGN KEY (hallmark_id) REFERENCES hallmark (id)
-);
-
-CREATE TABLE tf_crispr (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    gene_id integer unsigned,
-    cell_line varchar(20),
-    log2fc float,
-    fdr float,
-    PRIMARY KEY (id),
-    FOREIGN KEY (gene_id) REFERENCES gene (id)
+    FOREIGN KEY (exp_cond_id) REFERENCES exp_cond (id)
 );
 
 CREATE TABLE tf_regulator (
@@ -156,41 +115,5 @@ CREATE TABLE go_gene (
     PRIMARY KEY (id),
     FOREIGN KEY (go_bp_id) REFERENCES go_bp (id),
     FOREIGN KEY (gene_id) REFERENCES gene (id)
-);
-
-CREATE TABLE nci_nature_pathway (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    name text,
-    PRIMARY KEY (id)
-);
-
-CREATE TABLE nci_gene (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    nci_nature_pathway_id integer unsigned,
-    gene_id integer unsigned,
-    PRIMARY KEY (id),
-    FOREIGN KEY (nci_nature_pathway_id) REFERENCES nci_nature_pathway (id),
-    FOREIGN KEY (gene_id) REFERENCES gene (id)
-);
-
-CREATE TABLE somatic_mutation (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    ext_id integer,
-    mutation_type varchar(10),
-    PRIMARY KEY (id),
-    KEY idx_ext_id (ext_id)
-);
-
-CREATE TABLE causal_flow (
-    id integer unsigned NOT NULL AUTO_INCREMENT,
-    somatic_mutation_id integer unsigned,
-    regulator_id integer unsigned,
-    regulator_type varchar(10),
-    bicluster_id integer unsigned,
-    leo_nb_atob float,
-    mlogp_m_atob float,
-    PRIMARY KEY (id),
-    FOREIGN KEY (somatic_mutation_id) REFERENCES somatic_mutation (id),
-    FOREIGN KEY (bicluster_id) REFERENCES bicluster (id)
 );
 
